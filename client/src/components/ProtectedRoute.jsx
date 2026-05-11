ab4da04438a5f9dd75eaa70906659c5c6c418846
deraolisah <deraolisah@gmail.com>
2026-05-11 13:55:29 +0100

    Resolving Frontend Publishing

--- client/src/components/ProtectedRoute.jsx
+++ client/src/components/ProtectedRoute.jsx
@@ -1,7 +1,7 @@
 // components/ProtectedRoute.jsx
 import React from 'react';
 import { Navigate } from 'react-router-dom';
-import { useAuth } from '../contexts/AuthContext';
+import { useAuth } from '../contexts/authContext';
 
 const ProtectedRoute = ({ children }) => {
   const { isAuthenticated, loading } = useAuth();
